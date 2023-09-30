40ac60590546b9116e0e00543d66f2ec9bec4c8e
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-01 02:50:23 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, lang) {
+  return `Hi, my name is ${name} and I am learning to program in ${lang}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
